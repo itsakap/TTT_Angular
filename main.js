1a834c1745c9607613e2b73d4ee6c15e89a6ac0c
Adam Kaplan <AdamHKaplan@gmail.com>
2014-01-26 14:35:13 -0800

    adds transparent opponent on select pg, swaps arrow buttons for high heels, improves js

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var app = angular.module('TTT',["firebase"]);
   
 app.controller ('BoardCtrl', function($scope,$timeout,$firebase) {
 
-/*<FIREBASE LOGIC>*/
+
+/*<FIREBASE LOGIC (creates the multiplayer "cloud")>*/
 
   var ref = new Firebase("https://tictactohnoshebettadont.firebaseio.com/");//this is my fb
   $scope.fbRoot = $firebase(ref);
@@ -14,28 +15,20 @@ app.controller ('BoardCtrl', function($scope,$timeout,$firebase) {
     if(IDs.length == 0){  
       //no board --> let's build one!
       $scope.fbRoot.$add({
+
+        //the board's state and turns, shared by all users
         board:[['','',''],['','',''],['','','']],
         playerOnesTurn:true,
+        turns:0,
+        //player objects are in the cloud for access in directives, and so
+        //that players can VIEW each others' properties
         playerone:{charselection:0, nameIndex:0,piece:'x'},
         playertwo:{charselection:-100,nameIndex:1,piece:'o'},
-        turns:0,
+        
+        //helpers to sync and assign player
         xIsAvailable:true,
-        oIsAvailable:true,
-        names : [{name: "Milk"},
-                 {name: "April Carrion"},
-                 {name: "Vi Vacious"},
-                 {name: "Adore Delano"},
-                 {name: "Joslyn Fox"},
-                 {name: "Bianca del Rio"},
-                 {name: "Courtney Act"},
-                 {name: "Miss Darienne Lake"},
-                 {name: "Laganja Estranja"},
-                 {name: "Gia Gunn"},
-                 {name: "Magnolia Crawford"},
-                 {name: "Trinity K. Bonet"},
-                 {name: "Kelly Mantle"},
-                 {name: "Ben DeLaCreme"}
-                ]
+        oIsAvailable:true
+
       });
       $scope.fbRoot.$on("change",function(){
         IDs = $scope.fbRoot.$getIndex();
@@ -47,25 +40,51 @@ app.controller ('BoardCtrl', function($scope,$timeout,$firebase) {
       $scope.obj = $scope.fbRoot.$child(IDs[0]);
     }
   });
+//The created Firebase object ($scope.obj)
+//could contain more objects as values, if the
+//application is complex enough.
+/*< ! FIREBASE LOGIC>*/
 
-/*</FIREBASE LOGIC>*/
 
+
+/*<HELPERS (methods for UX)>*/
 
   $scope.pageToggle = 0;
+  $scope.names =["Milk",
+                 "April Carrion",
+                 "Vi Vacious",
+                 "Adore Delano",
+                 "Joslyn Fox",
+                 "Bianca del Rio",
+                 "Courtney Act",
+                 "Miss Darienne Lake",
+                 "Laganja Estranja",
+                 "Gia Gunn",
+                 "Magnolia Crawford",
+                 "Trinity K. Bonet",
+                 "Kelly Mantle",
+                 "Ben DeLaCreme"
+                ];
   $scope.start = function(){
     //"bind" this window to a player
     if(($scope.obj.xIsAvailable || $scope.obj.oIsAvailable) && $scope.pageToggle ==0){
       if($scope.obj.xIsAvailable){
         $scope.obj.xIsAvailable = false;
-        $scope.myPiece = 'x';
+        $scope.myPiece = {val:'x'};
       }
       else{
         $scope.obj.oIsAvailable  = false;
-        $scope.myPiece = 'o';
+        $scope.myPiece = {val:'o'};
       }
+      $scope.pageToggle++;
     $scope.obj.$save();
-  }
-  $scope.pageToggle++;
+    }
+  
+    else {
+      //show some sort of message like, A game is currently in progress;
+      $scope.pageToggle = 2;
+    }
+  
   };
 
   $scope.getStyle = function(c){
@@ -78,12 +97,13 @@ app.controller ('BoardCtrl', function($scope,$timeout,$firebase) {
     }
     //style is not returned if empty cell
   }
+//
+/*< ! HELPERS>*/
 
 
 
-  /*******************************/
- /*         <GAME LOGIC>        */
-/*******************************/
+/*<GAME LOGIC (methods for the game itself)>*/
+
 
 
   $scope.newGame = function(){
@@ -99,7 +119,7 @@ app.controller ('BoardCtrl', function($scope,$timeout,$firebase) {
     var piece = p ? 'x' : 'o';
 
     //if cell is empty and the piece I got equals piece predicated on player turn...
-    if(($scope.obj.board[c][r]=='') && ($scope.myPiece == piece)){
+    if(($scope.obj.board[c][r]=='') && ($scope.myPiece.val == piece)){
       $scope.obj.board[c][r] = piece;
       $scope.obj.turns++;
       $scope.obj.$save();
@@ -109,6 +129,7 @@ app.controller ('BoardCtrl', function($scope,$timeout,$firebase) {
   };
 
   function endTurn(c,r,p){
+    //initialize some possible win conditions as true, and search for counter-examples
     var horWin = true, vertWin = true, diag1Win = true, diag2Win = true, bd = $scope.obj.board, catsGame = ($scope.obj.turns==9);
     for (var i = 0; i < 3; i++){
       if(bd[i][r]!=p) horWin = false;
@@ -118,10 +139,10 @@ app.controller ('BoardCtrl', function($scope,$timeout,$firebase) {
     }
     if(horWin || vertWin || diag1Win || diag2Win) endGame(p + ' winned!');
     else if(catsGame) endGame('cats game :(');
-    else {
+    else /*new turn*/ {
       $scope.obj.playerOnesTurn = !$scope.obj.playerOnesTurn;
       $scope.obj.$save();
-    } //new turn
+    } 
   };
   function endGame (msg){
     //a slight delay which might not be necessary if we move that new game function
@@ -130,17 +151,11 @@ app.controller ('BoardCtrl', function($scope,$timeout,$firebase) {
       $scope.newGame();
     },500);
   }
-
-
-  /*******************************/
- /*         </GAME LOGIC>       */
-/*******************************/
 });
+/*< ! GAME LOGIC>*/
 
 
-
-
-
+/*<DIRECTIVES (methods for custom markup)>*/
 app.directive('intro',function(){
   return{
     restrict:"E",
@@ -155,14 +170,14 @@ app.directive('characters',function(){
     link:function(s){
 
       //we can initialize the style of the opposite carousel to be a little opaque!
-
+      
       //$watch function for background position of spritesheets
       s.$watch('player.charselection',function(n){
         s.carouselstyle = {backgroundPosition: n +"px 0px"};
       })
       //<carousel slider buttons>
       s.slide = function(direction){
-        if(s.player.piece == s.$parent.myPiece){
+        if(s.player.piece == s.$parent.myPiece.val){
           //new background position for sprite sheet
           var newPosition = (direction=='left') ? 1300 : 100;
           //new index in name array
@@ -174,7 +189,7 @@ app.directive('characters',function(){
           //send data to cloud
           s.$parent.$parent.obj.$save();
         }
-        else alert('YOU MAY NOT PASS');
+
       };
       //</carousel slider buttons>      
     }
@@ -194,3 +209,4 @@ app.directive('board',function(){
     }
   }
 })
+/*< ! DIRECTIVES>*/
